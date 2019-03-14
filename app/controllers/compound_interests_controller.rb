b3b286a9edf628a15f0a74b8a1df46fe5ad0bdb6
Jaryl Sim <jaryl@upcodeacademy.com>
2019-03-14 16:03:46 +0800

    Simplify routes and actions

--- app/controllers/compound_interests_controller.rb
+++ app/controllers/compound_interests_controller.rb
@@ -1,22 +1,14 @@
 class CompoundInterestsController < ApplicationController
 
   def show
-    @compound_interest = CompoundInterest.new
-  end
-
-  def create
     @compound_interest = CompoundInterest.new(compound_interest_params)
-    if @compound_interest.submit
-      render :show
-    else
-      render :show
-    end
+    @compound_interest.submit unless compound_interest_params.empty?
   end
 
   private
 
   def compound_interest_params
-    params.require(:compound_interest).permit(:current_principal, :years_to_grow, :interest_rate)
+    params.fetch(:compound_interest, {}).permit(:current_principal, :years_to_grow, :interest_rate)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root to: redirect('/compound_interest')
 
-  resource :compound_interest, only: [:show, :create]
+  resource :compound_interest, only: [:show]
 
 end
