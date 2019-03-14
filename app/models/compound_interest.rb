69a4422d32e934dd088530e54527652b4296074a
Jaryl Sim <jaryl@upcodeacademy.com>
2019-03-14 16:55:22 +0800

    Split up calculation for further improvements

--- app/models/compound_interest.rb
+++ app/models/compound_interest.rb
@@ -2,7 +2,7 @@ class CompoundInterest
 
   include ActiveModel::Model
 
-  attr_accessor :current_principal, :years_to_grow, :interest_rate
+  attr_accessor :current_principal, :years_to_grow, :interest_rate#, :annual_addition
 
   attr_reader :result
 
@@ -18,7 +18,15 @@ class CompoundInterest
   private
 
   def calculate_result
+    future_value_of_principal + future_value_of_additions
+  end
+
+  def future_value_of_principal
     current_principal.to_d * (1 + interest_rate.to_d / 100 / 12) ** (years_to_grow.to_d * 12)
+  end
+
+  def future_value_of_additions
+    0
   end
 
 end
